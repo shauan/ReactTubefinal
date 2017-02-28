cb7e84f681be20ff6ef75242a10fa95d5734cca2
Shauan Ferreira Leite <shauanf@gmail.com>
2017-02-28 09:07:08 -0500

    upload wireframe

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ class App extends Component {
   }
 
   render() {
-    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 100);
 
     return (
       <div className="container">
